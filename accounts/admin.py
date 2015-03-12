9fe7346366939f95b07a81a58b430a09a4a4ae57
John <j.o.h.n@alumni.ubc.ca>
2015-03-12 13:36:46 -0700

    fixed teacher list and admin list

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,3 +2,6 @@ from django.contrib import admin
 from accounts.models import *
 
 # Register your models here.
+admin.site.register(TeacherUser)
+admin.site.register(UserProfile)
+admin.site.register(TeachingAvailability)

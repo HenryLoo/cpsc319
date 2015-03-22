63a31965e843ba54586b9badb70cb1268843f99d
John <j.o.h.n@alumni.ubc.ca>
2015-03-22 14:11:24 -0700

    not processing userprofile if user is anonymous

--- aplus/context_processors.py
+++ aplus/context_processors.py
@@ -1,10 +1,13 @@
 from school_components.models import School, Period
 # The context processor function
 def school_period(request):
-	user_school = request.user.userprofile.school
-	schoollist = School.objects.all().order_by('title') #exclude did not work here always so to fiz bug added "if not current on user" on html
-	periodlist = Period.objects.filter(school=user_school).order_by('-id').reverse()
+        user = request.user
+        if not user.is_anonymous(): #assume that a user will only be anonymous on the login page, so no need schoollist etc.
+                user_school = request.user.userprofile.school
+                schoollist = School.objects.all().order_by('title') #exclude did not work here always so to fiz bug added "if not current on user" on html
+                periodlist = Period.objects.filter(school=user_school).order_by('-id').reverse()
 
-	return {
-        'school_list': schoollist, 'period_list': periodlist,
-    }
+                return {
+                'school_list': schoollist, 'period_list': periodlist,
+                }
+        return { }

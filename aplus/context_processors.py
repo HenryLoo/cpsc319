d155a7a42b4f00746b06f384ac73d73995e60699
Milena Mayumi Costa <milenamayumicosta@gmail.com>
2015-03-22 19:10:56 -0700

    bug fixed

--- aplus/context_processors.py
+++ aplus/context_processors.py
@@ -1,7 +1,12 @@
 from school_components.models import School, Period
 # The context processor function
 def school_period(request):
-	user_school = request.user.userprofile.school
+
+	if request.user.is_authenticated():
+		user_school = request.user.userprofile.school
+	else:
+		user_school = None
+
 	schoollist = School.objects.all().order_by('title') #exclude did not work here always so to fiz bug added "if not current on user" on html
 	periodlist = Period.objects.filter(school=user_school).order_by('-id').reverse()
 
--- populate.py
+++ populate.py
@@ -6,6 +6,9 @@ def populate():
     notification_performance = NotificationType.objects.create(notification_type='Performance', condition=50, content='Student performance is lowest than 50%')
     notification_assignment = NotificationType.objects.create(notification_type='Assignment', condition=3, content='Student did not deliver 3 assignments')
 
+    system_admin_user = User.objects.create_user(username='admin@email.com', email='admin@email.com', password='admin', first_name='Admin First Name', last_name='Admin Last Name')
+    system_admin_userprofile = UserProfile.objects.create(user= system_admin_user, phone='778-666-000', role='SYSTEM_ADMIN', period=None, school=None)
+
 if __name__ == '__main__':
 
     print ('Starting population script..')
@@ -13,6 +16,8 @@ if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aplus.settings')
 
     from dashboard.models import *
+    from accounts.models import *
+
     populate()
 
     print ('.. Done!')
